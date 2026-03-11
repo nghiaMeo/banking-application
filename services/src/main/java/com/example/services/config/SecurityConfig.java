fc8d6436e1eeb8a8082c6b185826ecc740903e56
Nguyen Huu Nghia <nghia181032@gmail.com>
2026-03-11 21:37:54 +0700

    security: build JWT and token

--- services/src/main/java/com/example/services/config/SecurityConfig.java
+++ services/src/main/java/com/example/services/config/SecurityConfig.java
@@ -2,8 +2,12 @@ package com.example.services.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
@@ -11,5 +15,22 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder(12);
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(authz -> authz.requestMatchers(
+                                        "/api/auth/**",            //Auth endpoints
+                                        "/api/users",              // Register
+                                        "/swagger-ui/**",           // Swagger UI
+                                        "/v3/api-docs/**",          // OpenAPI docs
+                                        "/swagger-ui.html"          // Swagger HTML
+                                ).permitAll()
+                                .anyRequest().authenticated()
+                ).formLogin(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable);
+        return http.build();
     }
 }
